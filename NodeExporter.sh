fda2a1719d57ede12e9856419a89c8cc98684091
Datta Rahegaonkar <dattarahegaonkar09@gmail.com>
2026-07-07 19:30:20 +0000

    Added: NodeExporter installation script

---
+++ NodeExporter.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# This script is used to install and start Node Exporter on a Linux system.
+
+cd /tmp
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+
+tar -xvf node_exporter-*.tar.gz
+
+cd node_exporter-*.linux-amd64
+
+sudo mv node_exporter /usr/local/bin/
+
+sudo useradd --no-create-home --shell /bin/false node_exporter
+
+sudo tee /etc/systemd/system/node_exporter.service <<EOF
+
+[Unit]
+Description=Node Exporter
+After=network.target
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+sudo systemctl status node_exporter
