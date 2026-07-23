a636dd4b36566895c35cb0f6428effa45401248d
Datta Rahegaonkar <dattarahegaonkar09@gmail.com>
2026-07-23 19:11:35 +0000

    Added: Jenkins installation script

--- Jenkins.sh
+++ Jenkins.sh
@@ -8,9 +8,12 @@ java -version
 
 # Setup Jenkins
 
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
 
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
 sudo apt install jenkins -y
